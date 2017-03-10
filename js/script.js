d8b5c46c127033483e31f6270166a9285a0181ce
David <hyhdvd@gmail.com>
2017-03-09 17:23:35 -0800

    migrate from lithium

---
+++ js/script.js
@@ -0,0 +1,92 @@
+$(document).ready(function(){
+
+    $('.play-reel').click(function() {
+        $('.show-reel').show().html('<iframe src="http://player.vimeo.com/video/130365813?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-reel').animate( { opacity : "0" } );
+    });
+
+    $('.play-andy').click(function() {
+        $('.show-andy').show().html('<iframe src="http://player.vimeo.com/video/44914209?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-andy').animate( { opacity : "0" } );
+    });
+
+    $('.play-bltzn').click(function() {
+        $('.show-bltzn').show().html('<iframe src="http://player.vimeo.com/video/148515810?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-bltzn').animate( { opacity : "0" } );
+    });
+
+    $('.play-cherry').click(function() {
+        $('.show-cherry').show().html('<iframe src="http://player.vimeo.com/video/92089181?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-cherry').animate( { opacity : "0" } );
+    });
+    $('.play-crossed').click(function() {
+        $('.show-crossed').show().html('<iframe src="http://player.vimeo.com/video/81585710?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-crossed').animate( { opacity : "0" } );
+    });
+
+    $('.play-hotel').click(function() {
+        $('.show-hotel').show().html('<iframe src="http://player.vimeo.com/video/109156986?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-hotel').animate( { opacity : "0" } );
+    });
+
+    $('.play-jeff').click(function() {
+        $('.show-jeff').show().html('<iframe src="http://player.vimeo.com/video/81579510?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-jeff').animate( { opacity : "0" } );
+    });
+
+    $('.play-love').click(function() {
+        $('.show-love').show().html('<iframe src="http://player.vimeo.com/video/81694767?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-love').animate( { opacity : "0" } );
+    });
+
+    $('.play-greenr').click(function() {
+        $('.show-greenr').show().html('<iframe src="http://player.vimeo.com/video/90444179?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-greenr').animate( { opacity : "0" } );
+    });
+
+    $('.play-drie1').click(function() {
+        $('.show-drie1').show().html('<iframe src="http://player.vimeo.com/video/133425293?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-drie1').animate( { opacity : "0" } );
+    });
+    $('.play-drie2').click(function() {
+        $('.show-drie2').show().html('<iframe src="http://player.vimeo.com/video/133884313?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-drie2').animate( { opacity : "0" } );
+    });
+    $('.play-drie3').click(function() {
+        $('.show-drie3').show().html('<iframe src="http://player.vimeo.com/video/133889615?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-drie3').animate( { opacity : "0" } );
+    });
+    $('.play-drie4').click(function() {
+        $('.show-drie4').show().html('<iframe src="http://player.vimeo.com/video/133596998?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-drie4').animate( { opacity : "0" } );
+    });
+
+    $('.play-yannis').click(function() {
+        $('.show-yannis').show().html('<iframe src="http://player.vimeo.com/video/112756959?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-yannis').animate( { opacity : "0" } );
+    });
+
+    $('.play-ceremony').click(function() {
+        $('.show-ceremony').show().html('<iframe src="http://player.vimeo.com/video/124992972?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-ceremony').animate( { opacity : "0" } );
+    });
+
+    $('.play-pantone1').click(function() {
+        $('.show-pantone1').show().html('<iframe src="http://player.vimeo.com/video/141634493?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-pantone1').animate( { opacity : "0" } );
+    });
+    $('.play-pantone2').click(function() {
+        $('.show-pantone2').show().html('<iframe src="http://player.vimeo.com/video/135386734?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-pantone2').animate( { opacity : "0" } );
+    });
+    $('.play-pantone3').click(function() {
+        $('.show-pantone3').show().html('<iframe src="http://player.vimeo.com/video/138679678?title=0&amp;byline=0&amp;portrait=0;color=8fffd1&amp;autoplay=1" class="vimeo" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>');
+        $('.preview-pantone3').animate( { opacity : "0" } );
+    });
+
+    $('.play-button').click(function() {
+        $(this).fadeOut(100);
+    } );
+
+}); // document ready
+
